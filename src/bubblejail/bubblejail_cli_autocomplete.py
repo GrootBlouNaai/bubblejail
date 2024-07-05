d9d4b3c14f4389621a25fc74bf6b23113beb65c5
RoboPatcher24 <108548496+GrootBlouNaai@users.noreply.github.com>
2024-07-05 10:13:17 +0200

    Update bubblejail_cli_autocomplete.py

--- src/bubblejail/bubblejail_cli_autocomplete.py
+++ src/bubblejail/bubblejail_cli_autocomplete.py
@@ -1,10 +1,8 @@
-# SPDX-License-Identifier: GPL-3.0-or-later
-# SPDX-FileCopyrightText: 2019-2023 igo95862
 from __future__ import annotations
 
 from argparse import ArgumentParser
 from shlex import split as shlex_split
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Iterable
 
 from .bubblejail_cli import (
     iter_instance_names,
@@ -32,77 +30,64 @@ class AutoCompleteParser:
         want_instance_set = {"edit", "run", "generate-desktop-entry"}
         base_options = {"--help", "--version"}
 
-        # enumerate words to allow LL parser lookahead
         enumer_words = enumerate(words)
-        _ = next(enumer_words)  # cycle 'bubblejail'
-        # 1. Parse base options (--help) and subcommands
-        while True:
-            index, token = next(enumer_words)
-            # If its an option autocomplete to base options
-            if token.startswith("-"):
-                self.last_auto_complete = base_options
-                continue
-            else:
-                # else it is probably a subcommand
-                subcommand = token
-                break
+        next(enumer_words)  # Skip 'bubblejail'
 
         try:
+            index, token = next(enumer_words)
+            while token.startswith("-"):
+                self.last_auto_complete = base_options
+                index, token = next(enumer_words)
+            subcommand = token
+
             subcommand_options = tuple(iter_subcommand_options(subcommand))
         except KeyError:
-            # Check if there are no arguments after this
             try:
-                _ = next(enumer_words)
+                next(enumer_words)
             except StopIteration:
-                # If this was the subcommand then give
-                # subcommands as completion variants
                 return
-            else:
-                # No auto-completion
-                self.last_auto_complete = tuple()
-
+            self.last_auto_complete = tuple()
+            return
+        except StopIteration:
             return
 
         subject_set = False
 
-        while True:
-            index, token = next(enumer_words)
+        try:
+            while True:
+                index, token = next(enumer_words)
 
-            if subject_set:
-                # if we set our subject (i.e. instance)
-                # extra arguments should not be completed
+                if subject_set:
+                    self.last_auto_complete = tuple()
+                    return
+
+                if token.startswith("-"):
+                    self.last_auto_complete = subcommand_options
+                    continue
+
+                if subcommand == "list":
+                    self.last_auto_complete = iter_list_choices()
+                    subject_set = True
+                    continue
+
+                if words[index - 1] == "--profile":
+                    self.last_auto_complete = BubblejailDirectories.iter_profile_names()
+                    continue
+
+                if subcommand in want_instance_set:
+                    self.last_auto_complete = iter_instance_names()
+                    subject_set = True
+                    continue
+
                 self.last_auto_complete = tuple()
-                return
-
-            if token.startswith("-"):
-                # Parse base options and subcommands
-                self.last_auto_complete = subcommand_options
-                continue
-
-            if subcommand == "list":
-                self.last_auto_complete = iter_list_choices()
-                subject_set = True
-                continue
-
-            if words[index - 1] == "--profile":
-                # Wants profile
-                self.last_auto_complete = BubblejailDirectories.iter_profile_names()
-                continue
-
-            if subcommand in want_instance_set:
-                # Wants instance name
-                self.last_auto_complete = iter_instance_names()
-                subject_set = True
-                continue
-
-            # Does not want anything
-            self.last_auto_complete = tuple()
+        except StopIteration:
+            return
 
     def auto_complete(self, current_cmd: str) -> Iterable[str]:
         try:
             self.auto_complete_parser(current_cmd)
         except StopIteration:
-            ...
+            pass
 
         yield from self.last_auto_complete
 
